70aa009240b2402e0e763c2529fd8173bd91fd62
paolo banez <paolo.banez@intechsive.com>
2020-02-09 00:09:28 +0800

    added 404, added article component

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Article } from "../article";
+import { ArticleService } from "../article.service";
 
 @Component({
-  selector: 'app-article-list',
-  templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  selector: "app-article-list",
+  templateUrl: "./article-list.component.html",
+  styleUrls: ["./article-list.component.scss"]
 })
 export class ArticleListComponent implements OnInit {
+  articles: Article[] = [];
 
-  constructor() { }
+  constructor(private articleService: ArticleService) {}
 
   ngOnInit() {
+    this.getArticles();
   }
 
+  getArticles(): void {
+    this.articleService
+      .getArticles()
+      .subscribe(articles => (this.articles = articles));
+  }
 }
