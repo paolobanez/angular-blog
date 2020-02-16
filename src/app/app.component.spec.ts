37b6ec62b9a0dd624363b1b032409776389d698d
paolo banez <paolo.banez@intechsive.com>
2020-02-17 01:05:21 +0800

    rename project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tour-heroes'`, () => {
+  it(`should have as title 'ng-blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tour-heroes');
+    expect(app.title).toEqual('ng-blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tour-heroes app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-blog app is running!');
   });
 });
